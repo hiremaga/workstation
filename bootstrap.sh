7cf189492790e5d58d8cfdd99438792fea6890ae
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-19 21:18:32 -0800

    Re-introduce SSH key generation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env sh
 
-## 1. XCode
+# 1. XCode
 read -p "Install XCode and press any key when you're done"
 
-## 2. Command Line Tools
+# 2. Command Line Tools
 read -p "Now download & install Command Line Tools from XCode > Preferences > Downloads"
 
-## 3. Temporary Clone
+# 3. SSH Key, needed for Github
+echo "Please enter your email: "
+read email
+ssh-keygen -t rsa -C "$email"
+cat ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub | pbcopy
+
+read -p "Your new SSH key has been copied to the clipboard add it to Github and press any key to continue"
+
+# 4. Temporary Clone
 cd /tmp
-git clone git://github.com/hiremaga/workstation.git
+git clone git@github.com:hiremaga/workstation.git
 cd workstation
 
-## 4. Install Soloist et al.
+# 5. Install Soloist et al.
 sudo gem install bundler
 bundle install --binstubs
 
-## 5. First Run
+# 6. First Run
 bin/soloist
