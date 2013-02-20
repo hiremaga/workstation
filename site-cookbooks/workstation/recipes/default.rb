49ec3883a5ad9b1460cee80ee5c60c763a13de5f
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-19 22:11:09 -0800

    Add git lol

--- site-cookbooks/workstation/recipes/default.rb
+++ site-cookbooks/workstation/recipes/default.rb
@@ -2,5 +2,6 @@ include_recipe 'pivotal_workstation::meta_osx_base'
 include_recipe 'pivotal_workstation::meta_osx_development'
 include_recipe 'pivotal_workstation::meta_ruby_development'
 
+include_recipe 'workstation::git_config'
 include_recipe 'workstation::packages'
 include_recipe 'workstation::git_config'
--- site-cookbooks/workstation/recipes/git_config.rb
+++ site-cookbooks/workstation/recipes/git_config.rb
@@ -1,3 +1,5 @@
 execute 'git config --global user.email "abhijit@hiremaga.com"'
 execute 'git config --global user.name "Abhi Hiremagalur"'
 execute 'git config --global push.default simple'
+execute "git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit'"
+execute "git config --global alias.lola 'log --graph --decorate --pretty=oneline --abbrev-commit -all'"
