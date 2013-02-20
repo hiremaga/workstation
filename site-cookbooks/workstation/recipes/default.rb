5fb57ece1a803d0b6def528569219101da1c3360
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-19 21:25:19 -0800

    Tell git about me

--- site-cookbooks/workstation/recipes/default.rb
+++ site-cookbooks/workstation/recipes/default.rb
@@ -3,3 +3,4 @@ include_recipe 'pivotal_workstation::meta_osx_development'
 include_recipe 'pivotal_workstation::meta_ruby_development'
 
 include_recipe 'workstation::packages'
+include_recipe 'workstation::git_config'
---
+++ site-cookbooks/workstation/recipes/git_config.rb
@@ -0,0 +1,3 @@
+execute 'git config --global user.email "abhijit@hiremaga.com"'
+execute 'git config --global user.name "Abhi Hiremagalur"'
+execute 'git config --global push.default simple'
