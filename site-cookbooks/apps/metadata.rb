f87e3e03d6aa2862c3aaf7d0fd24017657e034a8
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-19 22:59:39 -0800

    Instroduce apps cookbook with fitibit recipe

---
+++ site-cookbooks/apps/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        'Abhijit Hiremagalur'
+maintainer_email  'abhijit@hiremaga.com'
+license           'MIT'
+description       'Recipes for various OS X apps Abhi uses'
+version           '0.1'
+supports          'mac_os_x'
+depends           'dmg'
+
+recipe 'apps::fitbit', 'Install the Fitbit Sync app'
--- site-cookbooks/workstation/recipes/apps.rb
+++ site-cookbooks/workstation/recipes/apps.rb
@@ -1,2 +1,3 @@
 include_recipe 'pivotal_workstation::1password'
 include_recipe 'pivotal_workstation::pg_admin'
+include_recipe 'apps::fitbit'
