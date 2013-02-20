3868e44583acd65b9e49b0814601bd9b1f6b3a73
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-02-19 21:23:23 -0800

    Use default recipe instead of relying on soloistrc

---
+++ site-cookbooks/workstation/recipes/default.rb
@@ -0,0 +1,5 @@
+include_recipe 'pivotal_workstation::meta_osx_base'
+include_recipe 'pivotal_workstation::meta_osx_development'
+include_recipe 'pivotal_workstation::meta_ruby_development'
+
+include_recipe 'workstation::packages'
