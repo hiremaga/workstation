f6e06714db52b15b63e12772489045dc8c3bd6a1
Abhi Hiremagalur <abhijit@hiremaga.com>
2013-03-09 11:50:21 -0800

    install is the default action

--- site-cookbooks/apps/recipes/fitbit.rb
+++ site-cookbooks/apps/recipes/fitbit.rb
@@ -1,7 +1,6 @@
 dmg_package "Fitbit" do
   source "http://cache.fitbit.com/FitbitConnect/FitbitConnect_Mac_20121109_1.0.0.2286.dmg"
   volumes_dir "Fitbit Connect Installer-v1.0.0.2286-HadronRelease-Release-2012-11-09"
-  action :install
   type "pkg"
   package_id "com.fitbit.pkg.GalileoInstaller"
   checksum "23eb71a139368cb1259c9730cfc47adb6eb90fe416c59d2d165b320adef8d922"
